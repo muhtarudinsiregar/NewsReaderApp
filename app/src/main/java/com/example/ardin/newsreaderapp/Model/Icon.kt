b7f00f9a735147a02d9c0ee4fd1e578788fb06dd
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-28 13:44:21 +0700

    Added iconBetterIdea component

---
+++ app/src/main/java/com/example/ardin/newsreaderapp/Model/Icon.kt
@@ -0,0 +1,14 @@
+package com.example.ardin.newsreaderapp.Model
+
+/**
+ * Created by ardin on 27/02/18.
+ */
+data class Icon(
+        val url: String,
+        val width: Int,
+        val heigh: Int,
+        val bytes: Int,
+        val format: String,
+        val sha1sum: String,
+        val error: Any
+)
---
+++ app/src/main/java/com/example/ardin/newsreaderapp/Model/IconBetterIdea.kt
@@ -0,0 +1,7 @@
+package com.example.ardin.newsreaderapp.Model
+
+
+/**
+ * Created by ardin on 27/02/18.
+ */
+data class IconBetterIdea(val url: String, val icons: List<Icon>)
