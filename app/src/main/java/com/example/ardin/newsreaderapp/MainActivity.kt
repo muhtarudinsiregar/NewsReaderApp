7fba32c4d51e2ab31513e4ea142fd49569b307dd
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-28 13:44:49 +0700

    Integrate with adapter in Main

--- app/src/main/java/com/example/ardin/newsreaderapp/MainActivity.kt
+++ app/src/main/java/com/example/ardin/newsreaderapp/MainActivity.kt
@@ -1,12 +1,109 @@
 package com.example.ardin.newsreaderapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.example.ardin.newsreaderapp.Adapter.ListSourceAdapter
+import com.example.ardin.newsreaderapp.Common.Common
+import com.example.ardin.newsreaderapp.Interface.NewsService
+import com.example.ardin.newsreaderapp.Model.WebSite
+import com.google.gson.Gson
+import dmax.dialog.SpotsDialog
+import io.paperdb.Paper
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var newsService: NewsService
+    lateinit var adapter: ListSourceAdapter
+    lateinit var dialog: SpotsDialog
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        swipeRefresh.setOnRefreshListener {
+            loadWebsiteSource(true)
+        }
+
+        //init cache
+        Paper.init(this)
+
+        //init service
+        newsService = Common.getNewsService()
+
+        list_source.setHasFixedSize(true)
+        list_source.layoutManager = LinearLayoutManager(this)
+
+        dialog = SpotsDialog(this)
+
+        loadWebsiteSource(false)
+
+    }
+
+    private fun loadWebsiteSource(isRefreshed: Boolean) {
+        var cache: String? = null
+
+        if (!isRefreshed) {
+            cache = Paper.book().read("cache")
+
+            if (cache != null && !cache.isEmpty()) { //if have cache
+                val website: WebSite = Gson().fromJson(cache, WebSite::class.java)
+                val adapter = ListSourceAdapter(baseContext, website)
+
+                adapter.notifyDataSetChanged()
+                list_source.adapter = adapter
+            } else {
+                dialog.show()
+
+                newsService.getSources().enqueue(object : Callback<WebSite> {
+                    override fun onFailure(call: Call<WebSite>?, t: Throwable?) {
+
+                    }
+
+                    override fun onResponse(call: Call<WebSite>, response: Response<WebSite>) {
+                        adapter = ListSourceAdapter(baseContext, response.body() as WebSite)
+                        adapter.notifyDataSetChanged()
+
+                        list_source.adapter = adapter
+
+                        //save to cache
+                        Paper.book().write("cache", Gson().toJson(response.body()))
+
+                    }
+
+                })
+
+            }
+
+        } else {
+
+            newsService.getSources().enqueue(object : Callback<WebSite> {
+                override fun onFailure(call: Call<WebSite>?, t: Throwable?) {
+                    Log.d("MainActivity", "gagal getResources")
+                }
+
+                override fun onResponse(call: Call<WebSite>, response: Response<WebSite>) {
+                    adapter = ListSourceAdapter(baseContext, response.body() as WebSite)
+                    adapter.notifyDataSetChanged()
+
+                    list_source.adapter = adapter
+
+                    //save to cache
+                    Paper.book().write("cache", Gson().toJson(response.body()))
+
+                    //dismiss refresh progressing
+                    swipeRefresh.isRefreshing = false
+
+                }
+
+            })
+        }
     }
 }
