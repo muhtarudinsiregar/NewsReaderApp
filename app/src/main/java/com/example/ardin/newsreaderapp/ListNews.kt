d66fed173f47244d4bddff7ecab013f68cfefdef
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-03-01 17:30:21 +0700

    Skeleton detail news

---
+++ app/src/main/java/com/example/ardin/newsreaderapp/ListNews.kt
@@ -0,0 +1,12 @@
+package com.example.ardin.newsreaderapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListNews : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_news)
+    }
+}
